98dacd7e7255053c5e767916f90b45ccdbec5abe
Mariele Ponti <mpon3004@students.vsu.edu>
2021-04-16 18:09:33 -0400

    added functions to start random image (unsuccessful)

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ images[1] = "http://images.clipartpanda.com/square-clipart-jixEz4nET.png";
 images[2] = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Yellow_triangle.svg/878px-Yellow_triangle.svg.png";
 images[3] = "https://tse3.mm.bing.net/th?id=OIP.zanvgWxqEKLjTiyxEWOC9wHaHa&pid=Api";
 
-x = randomNumber(size), y = randomNumber(size);
+x = randomNumber(), y = randomNumber();
 function Hello() {
    alert("Hello, World");
 }
 
 /* takes the size of the array as argument. returns random number 
 * between 0 and size -1 */
-function randomNumber(size) { 
+function randomNumber() { 
     return Math.floor(Math.random() * size);
 } 
 
@@ -25,15 +25,25 @@ function onImageClick(winnerID, loserID) {
 
 // params so we know which image not to choose
 function switchImage(id1, id2) {
-    x = randomNumber(size)
+    x = randomNumber()
     while (document.getElementById("id1").src == images[x]){
-        x = randomNumber(size);
+        x = randomNumber();
     }
-    y = randomNumber(size)
+    y = randomNumber()
     while (y == x || document.getElementById("id2").src == images[y]){
-        y = randomNumber(size);
+        y = randomNumber();
     } 
     document.getElementById("id1").src = images[x];
     document.getElementById("id2").src = images[y];
     // loop through directory? while not id1 or id2...
+}
+
+function getRandomImage(){
+    var image = images[randomNumber()];
+    return image;
+}
+
+function displayRandomImage(){
+    var htmlImage = document.getElementById("randomImage");
+    htmlImage.src=getRandomImage;
 }
